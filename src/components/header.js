3395298fd49d9db83d5b6253e61439cf5dfd7a4c
Matt <unholyagent@gmail.com>
2018-11-26 08:59:31 +1000

    Added event handler to search bar

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+
+    inputChange(event){
+        console.log(event.target.value);
+    }
+
     render() {
         return(
             <header>
                 <div className = "logo">Logo</div>
-                <input/>
+                <input onChange={this.inputChange}/>
             </header>
         )
     }
